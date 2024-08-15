9d5856692e02e95c89d0447b221de4ecbf35a204
Pino <140602098+dokiet999@users.noreply.github.com>
2024-08-15 11:08:50 +0700

    Add files via upload

---
+++ J01001.java
@@ -0,0 +1,16 @@
+package jv;
+import java.util.*;
+
+public class J01001 {
+	public static Scanner ip = new Scanner(System.in);
+	public static void main(String[] args) {
+		int a = ip.nextInt();
+		int b = ip.nextInt();
+		int s = a * b;
+		int P = (a + b)*2;
+		System.out.print(P+" "+s);
+		
+		
+	}
+
+}
