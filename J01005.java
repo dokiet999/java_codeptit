ecc46ac9cf034653f45df552d1caa2000b7ff951
Pino <140602098+dokiet999@users.noreply.github.com>
2024-08-18 14:59:37 +0700

    Update J01005.java

--- J01005.java
+++ J01005.java
@@ -1,5 +1,17 @@
 package jv;
-
+import java.util.Scanner;
 public class J01005 {
-	
-}
+	public static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		int t = sc.nextInt();
+		while(t-- > 0) {
+			 int n = sc.nextInt();
+		        int h = sc.nextInt();
+		        for (double i = 1d; i < n; i++) {
+		            System.out.printf("%.6f ", Math.sqrt(i / n) * h);
+		        }
+		        System.out.println();
+		    }
+		}	
+		
+	}
