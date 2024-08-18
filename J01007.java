d340889cc696f207fd66c829bf1b84cb0c7e71b8
Pino <140602098+dokiet999@users.noreply.github.com>
2024-08-18 15:00:49 +0700

    Add files via upload

---
+++ J01007.java
@@ -0,0 +1,36 @@
+package jv;
+import java.util.Scanner;
+public class J01007 {
+	public static Scanner sc = new Scanner(System.in);
+	public static long[] fibonacci = new long[100];
+	public static void initialize() {
+		fibonacci[0] = 0;
+		fibonacci[1] = 1;
+		for(int i = 2 ; i < 100 ; i++) {
+			fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+		}
+	}
+	public static boolean checkFibo(long n) {
+		for(long i : fibonacci) {
+			if(i == n) {
+				return true;
+			}
+		}
+		return false;
+		
+	}
+	public static void main(String[] args) {
+		int t = sc.nextInt();
+		initialize();
+		while(t-- > 0) {
+			long n = sc.nextLong();
+			if(checkFibo(n)) {
+				System.out.println("YES");
+			}else {
+				System.out.println("NO");
+			}
+			
+		}
+		
+	}
+}
