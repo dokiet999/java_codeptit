6be77daeb475dfc800e85c7d7dcbea8ba4c24830
Pino <140602098+dokiet999@users.noreply.github.com>
2024-08-19 11:35:41 +0700

    Update J01005.java

--- J01005.java
+++ J01005.java
@@ -1,7 +1,11 @@
 package jv;
+
 import java.util.Scanner;
+
 public class J01005 {
+	
 	public static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		int t = sc.nextInt();
 		while(t-- > 0) {
