e663dfab029673dc42f02102f67c7e4940d8bce9
Pino <140602098+dokiet999@users.noreply.github.com>
2024-08-19 11:32:14 +0700

    Update J01001.java

--- J01001.java
+++ J01001.java
@@ -1,16 +1,17 @@
 package jv;
+
 import java.util.*;
 
 public class J01001 {
+	
 	public static Scanner ip = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		int a = ip.nextInt();
 		int b = ip.nextInt();
 		int s = a * b;
 		int P = (a + b)*2;
-		System.out.print(P+" "+s);
-		
-		
+		System.out.print(P+" "+s);	
 	}
 
 }
