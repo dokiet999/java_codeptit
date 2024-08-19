a633a3dffc106afe6c03a33d9b488a6dd2238996
Pino <140602098+dokiet999@users.noreply.github.com>
2024-08-19 11:33:23 +0700

    Update J01003.java

--- J01003.java
+++ J01003.java
@@ -1,10 +1,14 @@
 package jv;
+
 import java.util.Scanner;
+
 public class J01003 {
+
+	public static Scanner sc = new Scanner(System.in);	
+	
 	public static void main(String[] args) {
-		Scanner ip = new Scanner(System.in);
-		float a = ip.nextFloat();
-		float b = ip.nextFloat();
+		float a = sc.nextFloat();
+		float b = sc.nextFloat();
 		if(a >= 0 && b > 0) {
 			System.out.print("VN");
 		}else if(b == 0){
