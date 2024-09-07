f49705281c71944ea67a1fedf69024270c532260
Pino <140602098+dokiet999@users.noreply.github.com>
2024-09-07 11:36:22 +0700

    Update J03006.java

--- J03006.java
+++ J03006.java
@@ -7,8 +7,25 @@ public class J03006 {
 	public static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int t = sc.nextInt();
+		while (t-- > 0) {
+			String s = sc.next();
+			int n = s.length() - 1;
+			boolean flag = false;
+			for (int i = 0 ; i < n / 2; i++) {
+				char start = s.charAt(i);
+				char end = s.charAt(n- i);
+				if (start != end || start % 2 != 0) {
+					flag = true;
+					break;
+				}
+			}
+			if (flag == true) {
+				System.out.println("NO");
+			}else {
+				System.out.println("YES");
+			}
+		}
 	}
 
 }
